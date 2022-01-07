2d6e53970a5b1e28c880242181a2552420207a93
Jerry Tong <zihangtong@gmail.com>
2022-01-07 11:03:32 -0500

    more styling

--- src/components/TestCard.js
+++ src/components/TestCard.js
@@ -1,21 +1,56 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function TestCard({ test }) {
     const title = test.title.split(' ').join('');
 
     return (
-        <Card className='m-auto' style={{ width: '18rem', height: '10rem' }}>
+        <>
+        <style type='text/css'>
+        {`
+        .card {
+            box-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
+            transition: box-shadow 0.3s ease-in-out;
+        }
+        .card:hover{
+            box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+            // transition: box-shadow 0.3s ease-in-out;
+        }
+        // .card::after {
+        //     box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+        //     opacity: 0;
+        //     transition: opacity 0.3s ease-in-out;
+        // }
+        // .card:hover::after {
+        //     opacity: 1;
+        // }
+        `}
+        </style>
+
+        <Card as={Link} to={`/tests/${title}`} className='m-auto' style={{ width: '15rem', height: '18rem', textDecoration: 'none', }}>
+            <Card.Img variant="top" src={test.image} style={{width: '100%', height: '10rem', objectFit: 'cover'}}/>
             <Card.Body>
                 <Card.Title>{test.title}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{test.subtitle}</Card.Subtitle>
                 <Card.Text>
                 {test.description}
                 </Card.Text>
-                <Card.Link as={Link} to={`/tests/${title}`}>Play {test.title}</Card.Link>
+                {/* <Card.Link as={Link} to={`/tests/${title}`}>Play {test.title}</Card.Link> */}
             </Card.Body>
         </Card>
+
+        {/* <Card as={Link} to={`/tests/${title}`} className='m-auto' >
+        <Card.Img variant="top" src={test.image} />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+  </Card.Body>
+        </Card> */}
+        </>
     )
 }
 
--- src/components/TestCarousel.js
+++ src/components/TestCarousel.js
@@ -10,55 +10,71 @@ function TestsCarousel() {
   }
 
   return (
-    <div style={{margin: 'auto', width: '70%'}}>
+    <div>
       <style type="text/css">
         {`
         .carousel-item {
-          height: 400px;
-          max-width: 1500px;
-          min-width: 800px;
+          height: 600px;
+          background: #1a75ff;
         }
+
+        .carousel img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+        
         .carousel-caption {
           position: absolute;
-          top: 0%;
+          top: 38%;
+        }
+
+        .carousel-caption h2 {
+          background: rgba(0, 0, 0, 0.5);
+          padding: 1rem;
+          width: 70%;
+          margin: 0 auto;
+        }
+
+        .carousel-caption h5 {
+          background: rgba(0, 0, 0, 0.5);
+          width: 70%;
+          margin: 0 auto;
+          padding-bottom: 1rem;
         }
         `}
       </style>
 
-    <Carousel activeIndex={index} onSelect={handleSelect}>
+    <Carousel activeIndex={index} onSelect={handleSelect} fade={true} interval={5000}>
       <Carousel.Item as={Link} to='/tests/ReactionTest'>
         <img
-          className="d-block w-100"
           src="https://cdn.pixabay.com/photo/2014/11/16/23/39/superhero-534120_960_720.jpg"
           alt="Reaction Test slide"
         />
         <Carousel.Caption>
           <h2>Reaction Test</h2>
-          <p>Are you faster than a sloth?</p>
+          <h5>Are you faster than a sloth?</h5>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item as={Link} to='/tests/TypingTest'>
         <img
-          className="d-block w-100"
           src="https://media.npr.org/assets/img/2010/10/28/flyingfingers_wide-66394a3796b3a38b91f672f7ff84351dafa2f818.jpg"
           alt="Typing Test slide"
         />
-
         <Carousel.Caption>
-          <h3>Typing Test</h3>
-          <p>How good are you with your fingers?</p>
+          <h2>Typing Test</h2>
+          <h5>How good are you with your fingers?</h5>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item as={Link} to='/tests/NumberMemory'>
         <img
-          className="d-block w-100"
           src="https://media.wnyc.org/i/1500/1111/l/80/1/Numbers.png"
           alt="Number Memory slide"
         />
 
         <Carousel.Caption>
-          <h3>Number Memory</h3>
-          <p>Useful when a girl gives you her number.</p>
+          <h2>Number Memory</h2>
+          <h5>Useful when a girl gives you her number.</h5>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
